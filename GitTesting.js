2e715dce9fe53be39829dd1ac89b43f4e0e5d428
Deepak2255-MERN <yadavdeepak2255@gmail.com>
2024-09-13 12:10:54 +0530

    Added by Deepak

---
+++ GitTesting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const GitTesting = () => {
+  return <div>
+  <h3>  GitTesting </h3>
+  <p>This is my first task on Git, Please enlighten me for further understanding</p>
+    </div>;
+};
+
+export default GitTesting;
